c2c059beaa1ecb743c56946838aed0ee40077a67
RafiOISS <137367666+RafiOISS@users.noreply.github.com>
2023-06-23 01:15:45 +0600

    Add files via upload

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,3 +1,21 @@
 document.getElementById('dark-mode-toggle').addEventListener('click', function() {
   document.body.classList.toggle('dark-mode');
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  const navbarToggler = document.querySelector(".navbar-toggler");
+  const navbarMenu = document.querySelector(".navbar-collapse");
+  const navbarLinks = document.querySelectorAll(".nav-link");
+
+  navbarToggler.addEventListener("click", function() {
+    navbarMenu.classList.toggle("show");
+  });
+
+  navbarLinks.forEach(function(navbarLink) {
+    navbarLink.addEventListener("click", function() {
+      if (navbarMenu.classList.contains("show")) {
+        navbarMenu.classList.remove("show");
+      }
+    });
+  });
+});
